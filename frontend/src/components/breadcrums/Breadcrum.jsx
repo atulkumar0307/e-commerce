d7cd89452315faccb7f88dbe62ee1521c4851dc7
Atul Kumar <157139213+atulkumar0307@users.noreply.github.com>
2024-07-26 12:31:51 +0530

    Add files via upload

---
+++ frontend/src/components/breadcrums/Breadcrum.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Breadcrum.css'
+import arrow_icon from '../Assets/breadcrum_arrow.png'
+
+function Breadcrum(props) {
+  const {product} = props;
+
+  if (!product) {
+    return null; // or return a loading spinner or placeholder text
+  }
+
+  return (
+    <div className='breadcrum'>
+      HOME <img src={arrow_icon} alt='arrow_icon' />
+      SHOP <img src={arrow_icon} alt='arrow_icon' />
+      {product.category}<img src={arrow_icon} alt='arrow_icon' />{product.name}
+    </div>
+  )
+}
+
+export default Breadcrum
